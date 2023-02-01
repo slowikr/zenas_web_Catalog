e19cd270354bcf10c40a82894fe1959f2ccef80e
slowikr <36824502+slowikr@users.noreply.github.com>
2023-02-01 10:18:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,8 @@ import pandas
 streamlit.title("Zena's Amazing Athleisure Catalog") 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-color_or_style=my_cur.execute("select distinct color_or_style from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("select distinct color_or_style from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website")
+my_data_rows = my_cur.fetchall()
 
-
-option = streamlit.selectbox('How would you like to be contacted?', my_data_row)
+option = streamlit.selectbox('Pick a sweatsuit color or style', my_data_rows)
 streamlit.write('You selected:', option)
